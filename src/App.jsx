0660dd7eac9e51a775e537877bc5b8a4697e024e
Carlos Moura <carlos@email.com>
2024-09-10 19:06:32 -0300

    organiza as pastar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./Components/Footer";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
@@ -14,7 +14,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
