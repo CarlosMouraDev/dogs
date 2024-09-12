360a68a7f354a1768f4e72ec21eb698fcb1d476c
Carlos Moura <carlos@email.com>
2024-09-12 09:34:49 -0300

    Adiciona o botão de deletar uma foto caso o usuário seja o dono da postagem

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./PhotoContent.module.css";
 import PhotoComments from "./PhotoComments";
+import { useContext } from "react";
+import { UserContext } from "../../Contexts/UserContext";
+import PhotoDelete from "./PhotoDelete";
 
 export default function PhotoContent({ data }) {
   const { photo, comments } = data;
+  const user = useContext(UserContext);
 
   return (
     <div className={styles.photo}>
@@ -13,7 +17,11 @@ export default function PhotoContent({ data }) {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.views}>{photo.acessos}</span>
           </p>
           <h1 className="title">
---
+++ src/Components/Photo/PhotoDelete.jsx
@@ -0,0 +1,30 @@
+import { PHOTO_DELETE } from "../../Api/api";
+import useFetch from "../../Hooks/useFetch";
+import styles from "./PhotoDelete.module.css";
+
+export default function PhotoDelete({ id }) {
+  const { loading, req } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm("Tem certeza que quer deletar essa foto?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { res } = await req(url, options);
+      if (res.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+}
